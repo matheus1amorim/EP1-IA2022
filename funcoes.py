35b204892bd548133c651419534810a498981a8f
matheus1amorim <40796711+matheus1amorim@users.noreply.github.com>
2022-05-01 21:53:30 -0300

    Initial commit

---
+++ funcoes.py
@@ -0,0 +1,36 @@
+#####################################
+#Algoritmo Genetico Modificado#
+#EP 1 - Inteligencia Artificial#
+#Arquivo de Funcoes#
+
+#Funcao Gold#
+
+#Funcao SumS#
+
+#Funcao DeLJong#
+
+#Funcao Ackley#
+
+#Funcao Bump#
+
+#Funcao Rastringin#
+
+#Funcao Tradutora Binario para Real#
+
+#Funcao Tradutora Real para Binario#
+
+#Funcao Gera Populacao Inicial#
+
+#Funcao Calcula Fitness#
+
+#Funcao Calcula Melhor Individuo#
+
+#Funcao Calcula Media dos individuos#
+
+#Funcao Realiza Crossover#
+
+#Funcao Realiza Mutacao#
+
+#Funcao Realiza Ordenacao#
+
+#Funcao Calcula Estatistica#
---
+++ main.py
@@ -0,0 +1,33 @@
+#####################################
+#Algoritmo Genetico Modificado#
+#EP 1 - Inteligencia Artificial#
+
+#Definicao de parametros
+
+#Gera a Populacao Inicial
+
+#Calcula o Fitness
+
+#Calcula Melhor Individuo
+
+#Calcula Media dos individuos
+
+#Execução do AGM#
+
+#while gera=<Ngera
+#gera=gera+1
+#pop1 = Crossover(pop)
+#pop1 = mutacao(pop1)
+#pop2 = crossover(pop)
+#pop3 = mutacao(pop)
+#pop4 = aleatoria(chama mesma função da pop inicial)
+#pop_aux = concat(melhor,pop1,pop2,pop3,pop4)
+#fit = calc_fitness()
+#fit = ordena(fit)
+
+#[fmelhor,fmed]= calc_estatisticas(fit)
+
+#Gerar Saidas TXT E Graficos#
+
+
+
